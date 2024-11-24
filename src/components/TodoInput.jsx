46dbe28b705f2421350f69a136324a3306dad163
latikaguptak <08guptal@gmail.com>
2024-11-24 22:43:42 +0530

    Add the keypress feature in the todolist

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -5,8 +5,8 @@ import { MdAdd } from "react-icons/md";
 function TodoInput({ addTodo }) {
   const [todo, setTodo] = useState("");
 
-  const handleAdd = () => {
-    if (todo.trim()) {
+  const handleAdd = (e) => {
+    if (e.key === "Enter" && todo.trim()) {
       addTodo(todo);
       setTodo("");
     }
@@ -18,11 +18,13 @@ function TodoInput({ addTodo }) {
         type="text"
         value={todo}
         onChange={(e) => setTodo(e.target.value)}
+        onKeyDown={handleAdd}
         className="flex-1 border rounded-lg px-4 py-2 focus:outline-none focus:ring focus:ring-indigo-300"
         placeholder="Add a new to-do"
       />
       <button
         onClick={handleAdd}
+        
         className="bg-blue-600 text-white px-3 py-3 rounded-full shadow hover:bg-indigo-700
        text-xl">
         <MdAdd />
